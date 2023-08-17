d746709082e8086c2887cf5ee3d8dd7b469823ed
Marianita <mb.martinezgutierrez@ugto.mx>
2023-08-17 09:32:41 -0600

    Arrancamos el servidor

---
+++ index.js
@@ -0,0 +1,11 @@
+const http = require('http')
+const port = 5000
+
+const server = http.createServer((req, res) => {
+    res.end('Hola Alumnos')
+})
+
+
+server.listen(port, ( ) => {
+    console.log('Arranca el servidor')
+})
