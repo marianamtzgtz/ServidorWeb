ca49ef696f4458be52ccccd22692c0df5121a2a0
Marianita <mb.martinezgutierrez@ugto.mx>
2023-08-21 08:56:55 -0600

    Agregamos Plantillas

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-const http = require('http')
-const port = 5000
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000
 
-const server = http.createServer((req, res) => {
-    res.end('Hola Alumnos')
+//app.use(express.static(__dirname + '/public'))
+
+// Motor de Plantillas o Vistas
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        titulo: 'Index con EJS',
+        parametro: 'Hola'
+    })
 })
 
+app.get('/lenguajes', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html')
+})
 
-server.listen(port, ( ) => {
-    console.log('Arranca el servidor')
+app.listen(port, ( ) => {
+    console.log(`Arranca el servidor ${port}`)
 })
